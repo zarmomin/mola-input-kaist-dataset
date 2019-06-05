2a97bea2c99bff30bfa55dc0d1db44abd65d8342
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2019-06-05 17:44:05 +0200

    progress

--- include/mola-sensor-kaist-dataset/KaistDataset.h
+++ include/mola-sensor-kaist-dataset/KaistDataset.h
@@ -20,16 +20,48 @@
 #include <string>
 #include <variant>
 
-namespace mola
+namespace mola::kaist_dataset
 {
-namespace kaist_dataset
+/** \addtogroup mola_sensor_kaist_dataset_grp
+ * @{ */
+
+/** Loads a pose from a TXT in KAIST format. Throws on error. */
+mrpt::math::TPose3D load_pose_from_kaist_txt(const std::string& filename);
+
+struct SensorCamera
 {
+    std::string                  sensor_name;
+    std::string                  img_file_name;
+    uint8_t                      cam_idx;
+    mrpt::obs::CObservation::Ptr obs;
+};
+struct SensorIMU
+{
+    std::string                  sensor_name;
+    double                       wx, wy, wz, accx, accy, accz;
+    mrpt::obs::CObservation::Ptr obs;
+};
+struct SensorVelodyne
+{
+    std::string                  sensor_name;
+    mrpt::obs::CObservation::Ptr obs;
+};
+struct SensorSICK
+{
+    std::string                  sensor_name;
+    mrpt::obs::CObservation::Ptr obs;
+};
+using SensorEntry = std::variant<
+    std::monostate, SensorCamera, SensorIMU, SensorVelodyne, SensorSICK>;
+using kaist_timestamp_t = uint64_t;
+using kaist_dataset_t   = std::multimap<kaist_timestamp_t, SensorEntry>;
+
 /** RawDataSource from KAIST dataset.
  * Each "sequence" directory contains these sensor streams:
  * - xxx
  * - Ground truth poses
  *
- * \ingroup mola_sensor_kaist_dataset_grp */
+ */
 class KaistDataset : public RawDataSourceBase
 {
    public:
@@ -46,11 +78,19 @@ class KaistDataset : public RawDataSourceBase
     mrpt::Clock::time_point replay_begin_time_{};
     bool                    replay_started_{false};
     double                  time_warp_scale_{1.0};
-    std::array<mrpt::img::TCamera, 2>  cam_intrinsics_;
-    std::array<mrpt::math::TPose3D, 2> cam_poses_;  //!< wrt vehicle origin
+    bool                    publish_VLP_left{true}, publish_VLP_right{true};
+    bool                    publish_SICK_back{true}, publish_SICK_middle{true};
 
-    std::string seq_dir_;
+    // All poses are wrt vehicle frame of reference
+    // (refer to figures in KAIST paper)
+    mrpt::math::TPose3D pose_VLP_left_, pose_VLP_right_;
+
+    std::string seq_dir_;  //!< selected "sensor_data" directory
+
+    kaist_dataset_t           dataset_;  //!< dataset itself
+    kaist_dataset_t::iterator dataset_next_;  //!< next item to publish
 };
 
-}  // namespace kaist_dataset
-}  // namespace mola
+/** @} */
+
+}  // namespace mola::kaist_dataset
--- src/KaistDataset.cpp
+++ src/KaistDataset.cpp
@@ -19,11 +19,15 @@
 #include <mola-kernel/yaml_helpers.h>
 #include <mola-sensor-kaist-dataset/KaistDataset.h>
 #include <mrpt/core/initializer.h>
+#include <mrpt/io/vector_loadsave.h>
 //#include <mrpt/math/CMatrixTemplateNumeric.h>
 #include <mrpt/obs/CObservationIMU.h>
 #include <mrpt/obs/CObservationImage.h>
 #include <mrpt/system/filesystem.h>  //ASSERT_DIRECTORY_EXISTS_()
 #include <yaml-cpp/yaml.h>
+#include <Eigen/Dense>
+// Eigen must be before csv.h
+#include <mrpt/io/csv.h>
 
 using namespace mola;
 using namespace mola::kaist_dataset;
@@ -49,21 +53,63 @@ void KaistDataset::initialize(const std::string& cfg_block)
     YAML_LOAD_MEMBER_REQ(base_dir, std::string);
     YAML_LOAD_MEMBER_REQ(sequence, std::string);
 
-    seq_dir_ = base_dir_ + "/"s + sequence_ + "/mav0"s;
-    ASSERT_DIRECTORY_EXISTS_(seq_dir_);
-    // ASSERT_DIRECTORY_EXISTS_(seq_dir_ + "/cam0"s);
-
     // Optional params with default values:
     YAML_LOAD_MEMBER_OPT(time_warp_scale, double);
 
     // Preload everything we may need later to quickly replay the dataset in
     // realtime:
+    seq_dir_ = base_dir_ + "/"s + sequence_ + "/sensor_data"s;
+    ASSERT_DIRECTORY_EXISTS_(seq_dir_);
+
+    const auto calib_dir = base_dir_ + "/"s + sequence_ + "/calibration"s;
+    ASSERT_DIRECTORY_EXISTS_(calib_dir);
+
     MRPT_LOG_INFO_STREAM("Loading KAIST dataset from: " << seq_dir_);
 
+    // Sensor poses on the vehicle:
+    const auto calib_vlp_l = calib_dir + "/Vehicle2LeftVLP.txt"s;
+    pose_VLP_left_         = load_pose_from_kaist_txt(calib_vlp_l);
+
+    const auto calib_vlp_r = calib_dir + "/Vehicle2RightVLP.txt"s;
+    pose_VLP_right_        = load_pose_from_kaist_txt(calib_vlp_r);
+
+    MRPT_LOG_DEBUG_STREAM("Left VLP pose: " << pose_VLP_left_.asString());
+    MRPT_LOG_DEBUG_STREAM("Right VLP pose: " << pose_VLP_right_.asString());
+
     MRPT_TODO("continue");
+#if 0
+    // Velodynes: {0,1}
+    for (uint8_t cam_id = 0; cam_id < 2; cam_id++)
+    {
+        Eigen::Matrix<uint64_t, Eigen::Dynamic, Eigen::Dynamic> dat;
+
+        const auto cam_data_fil =
+            seq_dir_ + "/cam"s + std::to_string(cam_id) + "/data.csv"s;
+        ASSERT_FILE_EXISTS_(cam_data_fil);
+
+        mrpt::io::load_csv(cam_data_fil, dat);
+        ASSERT_(dat.cols() == 2);
+        ASSERT_(dat.rows() > 10);
+
+        SensorCamera se_cam;
+        se_cam.sensor_name = "cam"s + std::to_string(cam_id);
+        se_cam.cam_idx     = cam_id;
+
+        for (int row = 0; row < dat.rows(); row++)
+        {
+            const auto t = static_cast<euroc_timestamp_t>(dat(row, 0));
+            se_cam.img_file_name =
+                "/cam"s + std::to_string(cam_id) + "/data/"s +
+                std::to_string(static_cast<euroc_timestamp_t>(dat(row, 1))) +
+                ".png"s;
+
+            dataset_.emplace_hint(dataset_.end(), t, se_cam);
+        }
+    }
+#endif
 
     // Start at the dataset begin:
-    // dataset_next_ = dataset_.begin();
+    dataset_next_ = dataset_.begin();
 
     MRPT_END
 }  // end initialize()
@@ -86,5 +132,107 @@ void KaistDataset::spinOnce()
         mrpt::system::timeDifference(replay_begin_time_, mrpt::Clock::now()) *
         time_warp_scale_;
 
+    if (dataset_next_ == dataset_.end())
+    {
+        MRPT_LOG_THROTTLE_INFO(
+            10.0,
+            "End of dataset reached! Nothing else to publish (CTRL+C to quit)");
+        return;
+    }
+
+    // We have to publish all observations until "t":
+    while (dataset_next_ != dataset_.end() && t >= dataset_next_->first)
+    {
+        // Convert from dataset format:
+        const auto obs_tim =
+            mrpt::Clock::fromDouble(dataset_next_->first * 1e-9);
+
+        MRPT_TODO("continue");
+#if 0
+        std::visit(
+            overloaded{[&](std::monostate&) {
+                           THROW_EXCEPTION("Un-initialized entry!");
+                       },
+                       [&](SensorCamera& cam) {
+                           build_dataset_entry_obs(cam);
+                           cam.obs->timestamp = obs_tim;
+                           this->sendObservationsToFrontEnds(cam.obs);
+                           cam.obs.reset();  // free mem
+                       },
+                       [&](SensorIMU& imu) {
+                           build_dataset_entry_obs(imu);
+                           imu.obs->timestamp = obs_tim;
+                           this->sendObservationsToFrontEnds(imu.obs);
+                           imu.obs.reset();  // free mem
+                       }},
+            dataset_next_->second);
+#endif
+        // Advance:
+        ++dataset_next_;
+    }
+
+    // Read ahead to save delays in the next iteration:
+    {
+        ProfilerEntry tle(profiler_, "spinOnce.read_ahead");
+        //
+    }
+
+    MRPT_END
+}
+
+mrpt::math::TPose3D mola::kaist_dataset::load_pose_from_kaist_txt(
+    const std::string& filename)
+{
+    using namespace std::string_literals;
+
+    MRPT_START
+    ASSERT_FILE_EXISTS_(filename);
+
+    std::vector<std::string> lines;
+    if (!mrpt::io::loadTextFile(lines, filename))
+        THROW_EXCEPTION_FMT("Error reading filename: `%s`", filename.c_str());
+
+    // clang-format off
+    /** Example content:
+     *
+     * Left Velodyne (VLP-16) extrinsic calibration parameter from vehicle
+     * RPY(roll/pitch/yaw, degree), R(rotation matrix), T(translation matrix)
+     * RPY: 1.69162 44.9466 136.579
+     * R: -0.514066 -0.702201 -0.492595 0.486485 -0.711672 0.506809 -0.706447 0.0208933 0.707457
+     * T: -0.440699 0.397052 1.90953*
+     */
+    // clang-format on
+
+    ASSERT_(lines.size() >= 5);
+    ASSERT_(lines[3].substr(0, 2) == "R:"s);
+    ASSERT_(lines[4].substr(0, 2) == "T:"s);
+
+    const auto rot_str   = "["s + lines[3].substr(2) + "]"s;
+    const auto trans_str = "["s + lines[4].substr(2) + "]"s;
+
+    mrpt::math::CMatrixDouble rot_mat;
+    if (!rot_mat.fromMatlabStringFormat(rot_str))
+        THROW_EXCEPTION_FMT(
+            "Error parsing rotation matrix line: `%s`", lines[3].c_str());
+    ASSERT_EQUAL_(rot_mat.rows(), 1UL);
+    ASSERT_EQUAL_(rot_mat.cols(), 9UL);
+
+    mrpt::math::CMatrixDouble trans_mat;
+    if (!trans_mat.fromMatlabStringFormat(trans_str))
+        THROW_EXCEPTION_FMT(
+            "Error parsing translation line: `%s`", lines[4].c_str());
+    ASSERT_EQUAL_(trans_mat.rows(), 1UL);
+    ASSERT_EQUAL_(trans_mat.cols(), 3UL);
+
+    mrpt::math::CMatrixDouble33 R;
+    for (int i = 0; i < 9; i++) R(i) = rot_mat(0, i);
+
+    mrpt::poses::CPose3D p;
+    p.setRotationMatrix(R);
+    p.x(trans_mat(0, 0));
+    p.y(trans_mat(0, 1));
+    p.z(trans_mat(0, 2));
+
+    return p.asTPose();
     MRPT_END
 }
